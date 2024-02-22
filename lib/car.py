e79ab0496e8d92b3c99095bc14790db31abe47d4
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2024-02-22 17:05:40 +0300

    Completed Assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,14 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+
+    def honk(self):
+        print("Honk! Honk!")
+
+    def go(self):
+        return "The car goes vroom vroom!"
+
+    def fill_up_tank(self):
+        return "The car fills up its tank."
